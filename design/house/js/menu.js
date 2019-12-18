40a24e72d7c668fb4c214fc0c559f05e2762edb3
kiyotohiguchi <kiyoto011270@gmail.com>
2019-12-18 23:38:23 +0900

    編集rev2

---
+++ design/house/js/menu.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('.slider').slick({
+		centerMode: true,
+	    centerPadding: '0px',
+	    focusOnSelect:true,
+		slidesToShow:2,
+		slidesToScroll:2,
+		prevArrow:'<div class="slick-prev-button slick-button"><i class="fas fa-arrow-circle-left"></i></div>',
+        nextArrow:'<div class="slick-next-button slick-button"><i class="fas fa-arrow-circle-right"></i></div>',
+        responsive:[
+			{
+				breakpoint: 650,
+				settings:{
+				    slidesToShow:1,
+				    centerPadding: '10px',
+				}
+			},	
+		]
+	});
+
+	$('ul.menu').on('click',function(){
+		if ($('.menu-trigger').hasClass('active')){
+			$('.menu-trigger').removeClass('active');
+			$('nav').removeClass('active');
+		}else{
+			$('.menu-trigger').addClass('active');
+			$('nav').addClass('active');
+			console.log('.menu-nav');
+		}
+	});
+
+});
